5b13eb0ee6e35bcd604f433d020d76e738be23f3
Jimi <viidream@163.com>
2021-08-03 09:46:51 +0800

    数据库配置

---
+++ src/conf/db.js
@@ -0,0 +1,38 @@
+/*
+* 数据库配置文件
+* 作者：何俊
+* 创建时间：2021/08/03
+* 更新时间：2021/08/03
+* */
+
+// 环境参数，可以根据不同环境设置不同的参数配置
+const env = process.env.NODE_ENVSS
+
+let MYSQL_CONF
+
+// 开发环境
+if (env === 'development') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'xxxx',
+    password: 'xxxxx',
+    port: '3306',
+    database: 'xxxxx'
+  }
+}
+
+// 生产环境
+if (env === 'production') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'xxxx',
+    password: 'xxxxx',
+    port: '3306',
+    database: 'xxxxx'
+  }
+}
+
+
+module.exports = {
+  MYSQL_CONF
+}
