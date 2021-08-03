2d9b6a5c4e29bfa210b624f6995cbee51a446f99
Jimi <viidream@163.com>
2021-08-03 11:08:36 +0800

    封装xss和sql注入

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql')
 const {MYSQL_CONF} = require('../conf/db')
+const xss = require('xss')
 
 // 创建连接对象
 const con = mysql.createConnection(MYSQL_CONF)
@@ -19,9 +20,14 @@ function exec(sql) {
   })
 }
 
+// 防止xss和sql注入
+function prevent(value) {
+  xss(mysql.escape(value))
+}
+
 // 不进行断开连接，exec函数属于单例模式
 
 module.exports = {
   exec,
-  escape: mysql.escape // 防止SQL注入
+  prevent
 }
