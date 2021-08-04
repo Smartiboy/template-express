276eb9d3a6e86cd7bddf32bf519ac585a25f459d
Jimi <viidream@163.com>
2021-08-04 10:59:22 +0800

    新增自定义错误提示

--- src/schema/user.js
+++ src/schema/user.js
@@ -6,13 +6,25 @@
 const joi = require('joi')
 
 // 用户名
-const username = joi.string().pattern(/^[\u4e00-\u9fa5a-zA-Z]{2,12}$/).required()
+const username = joi.string().pattern(/^[\u4e00-\u9fa5a-zA-Z]{2,12}$/).required().messages({
+  'string.empty': '用户名不允许为空',
+  'any.required': '用户名为必填项',
+  'string.pattern.base': '用户名不符合校验规则',
+})
 
 // 密码
-const password = joi.string().pattern(/^[a-z0-9]{6,16}$/i).required()
+const password = joi.string().pattern(/^[a-z0-9]{6,16}$/i).required().messages({
+  'string.empty': '密码不允许为空',
+  'any.required': '密码为必填项',
+  'string.pattern.base': '密码不符合校验规则',
+})
 
 // 邮箱
-const email = joi.string().pattern(/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/).required()
+const email = joi.string().pattern(/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/).required().messages({
+  'string.empty': '邮箱不允许为空',
+  'any.required': '邮箱为必填项',
+  'string.pattern.base': '邮箱不符合校验规则',
+})
 
 // 注册规则
 register_schema = {
