13b04eb7f9d3d05a0869bf2171ba78be08279648
Jimi <viidream@163.com>
2021-08-07 09:57:13 +0800

    修改成功请求提示参数

--- src/controller/user.js
+++ src/controller/user.js
@@ -10,7 +10,7 @@ const register = async (req, res) => {
   // 查询用户名是否已被使用
   const result = await exec(`select username from users where username='${username}'`)
   if (result.length) {
-    return res.json(new SuccessModel(null, '用户名已存在'))
+    return res.json(new SuccessModel(null, '用户名已存在', req.method, 0))
   }
   // 密码加密
   let genPassword = bcrypt.hashSync(password, 10)
@@ -20,10 +20,10 @@ const register = async (req, res) => {
   try {
     const data = await exec(insertSql, [username, genPassword, 2, nowTime, nowTime, email])
     if (data.affectedRows.length) {
-      return res.json(new SuccessModel(null, '注册成功'))
+      return res.json(new SuccessModel(null, '注册成功', req.method))
     }
   } catch (e) {
-    return res.json(new SuccessModel(null, '注册失败'))
+    return res.json(new SuccessModel(null, '注册失败', req.method, 0))
   }
 }
 
